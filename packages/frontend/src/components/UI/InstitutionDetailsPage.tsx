18bf785a623b8d044ceb2cbe62c172d858c32a59
dt009715 <80246805+dt009715@users.noreply.github.com>
2025-03-03 08:15:59 +0100

    changement de la route institutions pour recuperer mes values

--- packages/frontend/src/components/UI/InstitutionDetailsPage.tsx
+++ packages/frontend/src/components/UI/InstitutionDetailsPage.tsx
@@ -1,5 +1,17 @@
+import ArtistDetailElement from "../organisms/ArtistDetailElement";
+import Footer from "../organisms/Footer";
+import Header from "../organisms/Header";
+
 const InstitutionDetailsPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="bg-background h-full">
+        <Header />
+        <ArtistDetailElement artistId={Number(id)} />
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default InstitutionDetailsPage;
